cdf06337f87422b0711a284aac624bb660e5d2d5
CaremelWaffle <tanachart.ar@gmail.com>
2018-12-05 01:31:36 +0800

    Add recommendation part

--- src/main/java/com/caramelwaffle/literature/controller/ResultController.java
+++ src/main/java/com/caramelwaffle/literature/controller/ResultController.java
@@ -1,6 +1,7 @@
 package com.caramelwaffle.literature.controller;
 
 import com.caramelwaffle.literature.model.dbl.DBLResult;
+import com.caramelwaffle.literature.recommendation.CollaborativeFiltering;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,18 @@ public class ResultController {
         model.addAttribute("keyword", name);
         if (!name.equals("")){
             DBLResult dblResult = callRestApi(name);
+            createRecommendation(dblResult);
             model.addAttribute("results", dblResult.getResult().getHits()
                     .getHit());
         }
         return "result";
+    }
+
+    private void createRecommendation(DBLResult dblResult) {
+        String currentUserId = "getUserId";
+        CollaborativeFiltering filtering = new CollaborativeFiltering();
+        filtering.setDataset(dblResult.getResult());
+        filtering.setRecommendationToUser(currentUserId);
     }
 
     private DBLResult callRestApi(String name) {
